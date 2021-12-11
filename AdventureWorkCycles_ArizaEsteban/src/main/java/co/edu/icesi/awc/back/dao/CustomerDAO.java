cf9ab3f370b6bd8bf0251594d0aad8751a99377a
VoodLyc <j.sebasgr036@gmail.com>
2021-12-11 15:02:59 -0500

    Added Store and Customer DAOs

---
+++ AdventureWorkCycles_ArizaEsteban/src/main/java/co/edu/icesi/awc/back/dao/CustomerDAO.java
@@ -0,0 +1,7 @@
+package co.edu.icesi.awc.back.dao;
+
+import co.edu.icesi.awc.back.model.sales.Customer;
+
+public class CustomerDAO extends AbstractJpaDAO<Customer, Integer> {
+
+}
---
+++ AdventureWorkCycles_ArizaEsteban/src/main/java/co/edu/icesi/awc/back/dao/StoreDAO.java
@@ -0,0 +1,7 @@
+package co.edu.icesi.awc.back.dao;
+
+import co.edu.icesi.awc.back.model.sales.Store;
+
+public class StoreDAO extends AbstractJpaDAO<Store, Integer> {
+
+}
